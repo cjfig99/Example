0b709fd78978b5516f6d6e9370ac0bd10f14e5a6
cjfig99 <connerfigal@q.com>
2017-05-15 12:39:14 -0600

    Checkpoint grade

--- src/run.java
+++ src/run.java
@@ -2,7 +2,14 @@
 public class run {
 
 	public static void main(String[] args) {
+
 		System.out.println("Bug is fixed");
+
+
+		System.out.println("Look at all these GUI's");
+		System.out.println("They worked");
+
+		System.out.println("Checkpoint Grade please");
 
 	}
 
